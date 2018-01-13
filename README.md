c189606374398c17d48fc83ae409060db3216406
Ravishankar Suribabu <Ravishankar.Suribabu@walmart.com>
2018-01-13 17:48:27 +0530

    Code check-in for practice

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# DataScience_Assignment_1.3
+# Problem Statement :
+```
+Write a Python program to accept the user's first and last name and then getting them
+printed in the the reverse order with a space between first name and last name.
+```
+
+## Snapshot - Run from jupyter REPL 
+```
+import get_input_names.ipynb in jupyter REPL 
+```
+
+![alt text](screenshots/get_input_names_jupyter_repl.png "Run from jupyter repl")
+
+
+## Snapshot - Run from command line bash
+```
+Command to execute : python get_input_names.py
+```
+
+![alt text](screenshots/get_input_names_command_line_bash.png "Run from command line bash")
---
+++ get_input_names.py
@@ -0,0 +1,35 @@
+"""
+Problem Statement :
+Write a Python program to accept the user's first and last name and then getting them
+printed in the the reverse order with a space between first name and last name.
+"""
+import re
+
+# Get Name Function - Generic
+def get_String_Input(prompt_message):
+    input_string = input(prompt_message)
+    return input_string
+
+
+# Validate to allow only string and spaces. No other characters allowed in First,last Names
+def validate_string(input_string):
+    return bool(re.search("^[a-zA-Z ]+$", input_string))
+
+
+# Function to loop input until correct string provided
+def loop_until_input(prompt_message):
+  while True:
+    userInput = get_String_Input(prompt_message).strip()
+    
+    if validate_string(userInput):
+        return userInput
+        break
+    else:
+        continue
+
+# Get Name details into variables
+first_name = loop_until_input("Enter First Name ")
+last_name = loop_until_input("Enter Last Name ")
+
+# print inputs 
+print (last_name , first_name)
